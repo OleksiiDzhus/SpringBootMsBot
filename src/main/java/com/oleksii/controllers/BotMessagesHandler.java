9fca163640306a92df0057a25063ebdfcbf78a26
Oleksii Dzhus <oleksa777@live.com>
2018-07-06 08:05:22 +0300

    BotMessagesHandler migrated to controllers package

--- src/main/java/com/oleksii/controllers/BotMessagesHandler.java
+++ src/main/java/com/oleksii/controllers/BotMessagesHandler.java
@@ -1,4 +1,4 @@
-package com.oleksii;
+package com.oleksii.controllers;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.joda.deser.DateTimeDeserializer;
